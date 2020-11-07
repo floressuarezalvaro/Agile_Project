190817acb57febd1f152e753b5461053abf5ac62
Franco <phanfranco@yahoo.com>
2020-11-07 13:39:50 -0800

    UPDATE:Added content into HTML file, added bulma styling.

---
+++ Assets/JS/results-app.js
@@ -0,0 +1,10 @@
+// var ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${key}`
+// fetch(ytUrl)
+// .then(function (response) {
+//     return response.json();
+// })
+// .then(function (data) {
+//     console.log(data);
+// });
+
+//https://developers.google.com/youtube/v3/docs/
