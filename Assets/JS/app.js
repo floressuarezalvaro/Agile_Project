901abccb0feb3ff47dda162850a38e74589a4fa4
Alvy <floressuarezalvaro@gmail.com>
2020-11-07 11:58:05 -0800

    added results page and fetch

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -76,3 +76,18 @@ console.log("hello world")
 
 
 //Alvaro 
+var key = 'AIzaSyDgOQIrGftzlp5IuWOtEiSU0Iw4tWvD_NE'
+//function searchYt(vid) {
+    var ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${key}`
+    fetch(ytUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+
+//}
+
+
+//https://developers.google.com/youtube/v3/docs/
