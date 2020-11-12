32d4c6971d500f6d5011a8b95de0d2c3abb1ba8e
Alvy <floressuarezalvaro@gmail.com>
2020-11-11 17:23:17 -0800

    seperated objects and trying to push

--- Assets/JS/results-app.js
+++ Assets/JS/results-app.js
@@ -1,4 +1,5 @@
 var searchForm = document.querySelector('#country-form');
+var ytContent = document.querySelector('#topresults')
 var key = 'AIzaSyDgOQIrGftzlp5IuWOtEiSU0Iw4tWvD_NE'
 
 function getParams() {
@@ -8,20 +9,39 @@ function getParams() {
     // Get the country
     var Country = searchCountryArr[0].split('=').pop();
     searchYoutube(Country)
-    console.log(Country)
 }
 
-getParams()
 
 function searchYoutube (Country) {
     var ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=${Country}&key=${key}`
-    // var ytUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${key}`
     
     fetch(ytUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            if (!data.items.length) {
+                console.log('No Youtube Videos Found');
+            } else {
+                console.log(ytContent)
+                ytContent.textContent = '';
+                for (let i = 0; i < data.items.length; i++) {
+                    showVids(data.items[i]);
+                }
+            }
+        })
+        // .catch(function (error) {
+        //     console.error(error);
+        // });
 };
+
+function showVids (itemsObj) {
+    console.log(itemsObj)
+    let ytDiv = document.createElement('div');
+    let ytBody = document.createElement('p');
+    ytContent.innerHTML = '<strong>ID:</strong> ' + itemsObj.id + '<br/>';
+}
+
+getParams()
